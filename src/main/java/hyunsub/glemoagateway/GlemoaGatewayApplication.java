2fb34600ac54939c5223707b26096c6a6eda04f5
신현섭 <didakd22@naver.com>
2025-09-30 22:20:14 +0900

    first commit

---
+++ src/main/java/hyunsub/glemoagateway/GlemoaGatewayApplication.java
@@ -0,0 +1,13 @@
+package hyunsub.glemoagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlemoaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlemoaGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hyunsub/glemoagateway/GlemoaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package hyunsub.glemoagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlemoaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
